9cb54d1a2410ac308aa67f0ef4d36ca696f4e8ad
Jared Andrews <jared.andrews07@gmail.com>
2024-11-20 00:58:56 -0600

    internal doc fix

--- R/original_ROSE.R
+++ R/original_ROSE.R
@@ -85,7 +85,7 @@ calculate_cutoff <- function(inputVector, drawPlot = TRUE, ...) {
 #'
 #' @keywords internal
 #' @author Young Lab, with documentation by Jared Andrews
-#' @rdname INTERNAL_calculate_cutoff
+#' @rdname INTERNAL_numPts_below_line
 numPts_below_line <- function(myVector, slope, x) {
     yPt <- myVector[x]
     b <- yPt - (slope * x)
